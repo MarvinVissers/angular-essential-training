05371a797bd18bf5ae4decdddce7d5272b1193c4
MarvinVissers <marvin.vissers@blokweg.nl>
2022-04-30 12:14:53 +0200

    Finished 2.1

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Appcomponent } from '.app.component';
+
+@NgModule({
+    import: [
+        BrowserModule
+    ],
+    declarations: [
+        Appcomponent
+    ],
+    bootstrap: [
+        Appcomponent
+    ]
+})
+
+export class AppModule {
+    
+}
