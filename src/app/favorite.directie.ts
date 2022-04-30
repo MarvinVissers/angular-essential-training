cad2855478705a993ad6fa7ec303512b64ac821d
MarvinVissers <marvin.vissers@blokweg.nl>
2022-04-30 14:59:57 +0200

    Finished 3.4

---
+++ src/app/favorite.directie.ts
@@ -0,0 +1,8 @@
+import { Directive, Host, HostBinding } from "@angular/core";
+
+@Directive({
+    selector: '[mwFavorite]'
+})
+export class FavoriteDirective {
+    @HostBinding('class.is-favorite') isFavorite = true;
+}
