b8e71d6a4c02b044b0fef0ff890c9c8993032538
MarvinVissers <32897103+MarvinVissers@users.noreply.github.com>
2022-05-01 11:34:12 +0200

    Finished 8.3

--- src/app/category-list.component.ts
+++ src/app/category-list.component.ts
@@ -8,7 +8,21 @@ import { Component, Input } from "@angular/core";
     </span>
   `,
   styles: [
-    ``
+    `
+      span {
+        display: inline-block;
+        margin: 0 4px 4px 0;
+      }
+
+      .label {
+        font-style: italic;
+        background-color: #999;
+        color: #fff;
+        padding: 2px 6px;
+        border-radius: 4px;
+        width: max-content;
+      }
+    `
   ]
 })
 export class CategoryListComponent {
